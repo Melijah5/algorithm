d6bd303eb30f87274c916ae1b3925fa7c090e925
Elias W <42394402+Melijah5@users.noreply.github.com>
2021-04-24 21:42:18 -0700

    Update algo_practice.py

--- algo_practice.py
+++ algo_practice.py
@@ -63,3 +63,11 @@ def log_day(num1, num2):
         print ("how did you know?")
     else:
         print("Just another day ...")
+        
+# Get a string made of its first three characters of a specified string
+def first_three(str):
+	return str[:3] if len(str) > 3 else str
+
+print(first_three('ipy'))
+print(first_three('python'))
+print(first_three('py'))
