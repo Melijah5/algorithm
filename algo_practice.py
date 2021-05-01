7c8cb18943ac714b8afb193ea9ade15b155b58a8
Elias W <42394402+Melijah5@users.noreply.github.com>
2021-04-30 23:06:10 -0700

    Update algo_practice.py

--- algo_practice.py
+++ algo_practice.py
@@ -91,3 +91,17 @@ def encode(str):
         }
     return output
 }
+
+# bubble_sort([0, 5, 2, 3, 2])
+#     [0, 2, 2, 3, 5]
+	
+    length = len(collection)
+    for i in range(length - 1):
+        swapped = False
+        for j in range(length - 1 - i):
+            if collection[j] > collection[j + 1]:
+                swapped = True
+                collection[j], collection[j + 1] = collection[j + 1], collection[j]
+        if not swapped:
+            break  # Stop iteration if the collection is sorted.
+    return collection
